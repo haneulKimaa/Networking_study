50fde509d656b39089892f470db7934ed27aba2a
haneulKimaa <khnpoq@naver.com>
2022-11-17 20:15:19 +0900

    [#1] baseURL, URLComponents 학습

--- NetworkingStudy/NetworkingStudy/ViewController.swift
+++ NetworkingStudy/NetworkingStudy/ViewController.swift
@@ -7,12 +7,26 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    // MARK: - Function
+    private func aboutBaseURL() {
+        let url = URL(string: baseURLString)
+        let relativeURL = URL(string: pathString, relativeTo: url)
+        print(relativeURL?.baseURL) // https://openapi.naver.com/v1
+    }
+    
+    private func aboutURLComponents() {
+        var urlComponents = URLComponents(string: baseURLString)
+        let query = URLQueryItem(name: "query", value: "가")
+        urlComponents?.queryItems?.append(query)
+    }
+    
 
 
 }
